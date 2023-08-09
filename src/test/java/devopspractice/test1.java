ade8ba2c0d6e3b07ce9ec6708e586f6176e79811
mulag <mulag@LAPTOP-V9M8NDSH>
2023-08-09 15:17:09 +0530

    committing as a manager

---
+++ src/test/java/devopspractice/test1.java
@@ -0,0 +1,11 @@
+package devopspractice;
+
+import org.testng.annotations.Test;
+
+public class test1 {
+    @Test
+    public void method() {
+        System.out.println("Hello sleepers");
+    }
+}
+
